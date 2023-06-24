699e69e75616462b0038d3cbd6111ba273f104d1
mingadinga <minpearl0826@gmail.com>
2023-06-24 11:21:35 +0900

    Documentation, REST to GraphQL

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 # 소개
 - 자료 : 노마드코더 **<GraphQL로 영화 API 만들기>**
-- 공부 기간 : 6월 21일 ~ 6월 -일
+- 공부 기간 : 6월 21일 ~ 6월 24일
 - 목표 : GraphQL 기본 이해, Apollo로 간단한 GraphQL 서버 만들기
 
 # 공부 기록
@@ -13,3 +13,4 @@
 | 2023-06-21 | REST의 문제와 GraphQL 소개 - UnderFetch, OverFetch                      |
 | 2023-06-22 | Query, Mutable, Non nullable                      |
 | 2023-06-23 | Type Resolver                      |
+| 2023-06-24 | Documentation, REST to GraphQL                      |
--- server.js
+++ server.js
@@ -33,8 +33,14 @@ const typeDefs = gql`
     id: ID!
     firstName: String!
     lastName: String!
+    """
+    Is the sum of firstName + lastName
+    """
     fullName: String!
   }
+  """
+  Tweet object represents a resource for a Tweet
+  """
   type Tweet {
     id: ID!
     text: String!
@@ -43,12 +49,44 @@ const typeDefs = gql`
   type Query {
     allUsers: [User!]!
     allTweets: [Tweet!]!
+    allMovies: [Movie!]!
     tweet(id: ID!): Tweet!
+    movie(id: String!): Movie!
     ping: String!
   }
   type Mutation {
     postTweet(text: String, userId: ID): Tweet
+    """
+    Deletes a Tweet if found, else return false
+    """
     deleteTweet(id: ID): Boolean
+  }
+  type Movie {
+    id: Int!
+    url: String!
+    imdb_code: String!
+    title: String!
+    title_english: String
+    title_long: String
+    slug: String
+    year: Int
+    rating: Int
+    runtime: Int
+    summary: String
+    description_full: String
+    synopsis: String
+    yt_trailer_code: String
+    language: String
+    mpa_rating: String
+    background_image: String
+    background_image_original: String
+    small_cover_image: String
+    medium_cover_image: String
+    large_cover_image: String
+    state: String
+    date_uploaded: String
+    date_uploaded_unix: Int
+    genres: [String]!
   }
 `;
 
@@ -60,11 +98,21 @@ const resolvers = {
     tweet(root, { id }) {
       return tweets.find(tweet => tweet.id === id);
     },
+    movie(_, {id}) {
+      return fetch(`https://yts.torrentbay.net/api/v2/movie_details.json?movie_id=${id}`)
+      .then((r) => r.json())
+      .then(json => json.data.movie);  
+    },
     allTweets() {
       return tweets;
     },
     allUsers() {
       return users;
+    },
+    allMovies() {
+      return fetch("https://yts.torrentbay.net/api/v2/list_movies.json")
+      .then((r) => r.json())
+      .then(json => json.data.movies);  
     }
   },
   Mutation: {
